f275e6a4ad8cb43a929204cdee30a9a89fe9de1a
www-data <www-data@ip-10-243-123-214.ec2.internal>
2013-07-27 14:58:24 +0000

    dig updates

--- dig/index.php
+++ dig/index.php
@@ -2,10 +2,18 @@
 
 $domain = $_GET['d'];
 $type = ( (isset($_GET['t'])) ? $_GET['t'] : 'A');
-$short = ( (isset($_GET['short'])) ? '' : '+short');
+$short = ( (isset($_GET['detail'])) ? '' : '+short');
 
 exec("dig $type $domain $short", $output);
 
 echo "<pre>" . implode('<br>', $output) . "</pre>";
+
+if(isset($_GET['www'])) {
+	echo "<b>### www ###</b><br>";
+
+	exec("dig $type www.$domain $short", $wwwoutput);
+
+	echo "<pre>" . implode('<br>', $wwwoutput) . "</pre>";
+}
 
 ?>
